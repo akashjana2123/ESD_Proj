b82791107184fda3fd61c3e4be4474beffc34427
_akash.jana__ <102944323+akashjana2123@users.noreply.github.com>
2025-07-02 12:30:51 +0530

    home page made and redirection done in js

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+//Handle Sin in
+const signInForm = document.getElementById('sign-in-form');
+signInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const emailInput = signInForm.querySelector('input[type="email"]').value.trim();
+    const passwordInput = signInForm.querySelector('input[type="password"]').value.trim();
+
+    if (emailInput === 'test@123.com' && passwordInput === 'password') {
+        window.location.href = 'home.html';
+    } else {
+        alert('Invalid email or password!');
+    }
+});
